da3f4a76c58630669dd4bb2bc52f4b8a70775a5f
Raul <rhc0001@alu.medac.es>
2022-02-14 17:28:08 +0100

    bot youtube

--- index.php
+++ index.php
@@ -37,12 +37,12 @@ function sendMessage($chatId, $response) {
 
 function keyBot ($chatId){
     $keyBot = 'AIzaSyAvWKk9QNoGiBPj7vhFtTO6kN4ZnVppumc';
-    $url = 'https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$canal.'&maxResults='.$maximo.'&key='.$keyBot;
+    $url = 'https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$canal.'&maxResults='.$maximo.'&key='.$keyBot.'';
     $canal = 'UCPsopTKQfSgW9XdYkKA6Gdw';
     $maximo = 5;
 
     
-    $apiData = file_get_contents($url);
+    $resultado = file_get_contents($url);
     $json = json_encode($url);
     $array = json_decode($json,TRUE);
 
