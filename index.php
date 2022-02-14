b69b13ef05ed5f1f75ec930b23bbc12f96b47afb
Raul <rhc0001@alu.medac.es>
2022-02-14 20:36:14 +0100

    bot auron9

--- index.php
+++ index.php
@@ -46,11 +46,13 @@ function keyBot ($chatId){
     $hola = json_decode($resultado,true);
     // $etag = $hola['etag'];
     // sendMessage($chatId,$etag);
-    for($i = 0 ; $i < 5 ; $i++){
-        // $video = $resultado['items'][$i]['id']['videoId'];
-        $urlVideo = "https://www.youtube.com/watch?v=".$resultado['items'][$i]['id']['videoId'];
+    $urlVideo = "https://www.youtube.com/watch?v=".$resultado['items'][0]['id']['videoId'];
         sendMessage($chatId,$urlVideo);
-    }
+    // for($i = 0 ; $i < 5 ; $i++){
+    //     // $video = $resultado['items'][$i]['id']['videoId'];
+    //     $urlVideo = "https://www.youtube.com/watch?v=".$resultado['items'][$i]['id']['videoId'];
+    //     sendMessage($chatId,$urlVideo);
+    // }
     
 }
 
