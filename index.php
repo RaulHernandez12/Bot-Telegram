041b7b4b25e75c9dfe6a12bcad3399c970411b22
Raul <rhc0001@alu.medac.es>
2022-02-15 19:43:05 +0100

    QUE PASA

--- index.php
+++ index.php
@@ -45,7 +45,7 @@ function keyBot ($chatId){
     $resultado = file_get_contents($url);
     $hola = json_decode($resultado,true);
     // sendMessage($chatId,$urlVideo);
-    for($i = 0 ; $i <= 5 ; $i++){
+    for($i = 0 ; $i < 5 ; $i++){
         // $video = $resultado['items'][$i]['id']['videoId'];
         $urlVideo = "https://www.youtube.com/watch?v=".$hola['items'][$i]['id']['videoId'];
         sendMessage($chatId,$urlVideo);
