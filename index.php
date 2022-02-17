644710e2b397a1ab13dd2404aeaa82965292ff51
Raul <rhc0001@alu.medac.es>
2022-02-17 17:01:16 +0100

    QUE PASA

--- index.php
+++ index.php
@@ -8,30 +8,37 @@ $update = json_decode($input, TRUE);
 $chatId = $update['message']['chat']['id'];
 $message = $update['message']['text'];
 
-switch($message) {
-    case '/start':
-        $response = 'Me has iniciado';
-        sendMessage($chatId, $response,FALSE);
-        break;
-    case '/info':
-        $response = 'Hola! Soy un bot de telegram';
-        sendMessage($chatId, $response,TRUE);
-        break;
-    case '/adios':
-        $response = 'Hasta luego';
-        sendMessage($chatId, $response,FALSE);
-        break;
-    case '/video':
-        keyBot($chatId);
-        break;
-    case '/canal':
-        $response = 'Que canal quieres ver?';
-        sendMessage($chatId, $response,TRUE);
-        break;
-    default:
-        $response = 'No te he entendido';
-        sendMessage($chatId, $response,FALSE);
-        break;
+if($repl == FALSE){
+    switch($message) {
+        case '/start':
+            $response = 'Me has iniciado';
+            sendMessage($chatId, $response,FALSE);
+            break;
+        case '/info':
+            $response = 'Hola! Soy un bot de telegram';
+            sendMessage($chatId, $response,FALSE);
+            break;
+        case '/adios':
+            $response = 'Hasta luego';
+            sendMessage($chatId, $response,FALSE);
+            break;
+        // case '/video':
+        //     keyBot($chatId);
+        //     break;
+        case '/canal':
+            $response = 'Que canal quieres ver?';
+            sendMessage($chatId, $response,TRUE);
+            break;
+        default:
+            $response = 'No te he entendido';
+            sendMessage($chatId, $response,FALSE);
+            break;
+    }
+}else {
+    switch($video){
+        case 'auron':
+            keyBot($chatId);
+    }
 }
 
 
